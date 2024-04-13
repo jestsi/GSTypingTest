b1ac6eff32cbfc4758538d4baadd093bf8f19427
Gest Se <gestjobm@gmail.com>
2024-04-13 22:29:10 +0300

    1

---
+++ console/migrations/m240413_192851_add_100_first_texts.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m240413_192851_add_100_first_texts
+ */
+class m240413_192851_add_100_first_texts extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m240413_192851_add_100_first_texts cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m240413_192851_add_100_first_texts cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
